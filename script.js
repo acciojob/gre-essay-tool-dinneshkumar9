2ba979c0b344616ca7717c09a37a228fc68d1ba4
Dinesh kumar <dinneshkumar9@gmail.com>
2023-01-22 02:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+const textbox = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+textbox.addEventListener("input", updateWordCount);
+
+function updateWordCount() {
+  const text = textbox.value;
+  const wordArray = text.split(" ");
+  const count = wordArray.filter(word => word.length > 0).length;
+  wordCount.innerHTML = count;
+}
